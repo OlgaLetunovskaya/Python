32281bf3010ce3934a3df282203bf0d1737ea5c7
Ольга Летуновская <los12ru@internet.ru>
2024-03-06 14:40:48 +0300

    Update lesson_5_task_5.py

--- lesson5/lesson_5_task_5.py
+++ lesson5/lesson_5_task_5.py
@@ -11,19 +11,20 @@ driver = webdriver.Chrome()
 driver.get("http://uitestingplayground.com/classattr")
 
 # Кликаем на синюю кнопку
-blue_button = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn class2 btn-primary btn-test")))
+blue_button = WebDriverWait(driver, 30).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn-primary")))
 blue_button.click()
 
 # Запускаем скрипт 3 раза подряд
 for i in range(3):
     driver = webdriver.Chrome()
     driver.get("http://uitestingplayground.com/classattr")
-    blue_button = driver.find_element(By.CSS_SELECTOR, ".btn class2 btn-primary btn-test")
+    blue_button = driver.find_element(By.CSS_SELECTOR, ".btn-primary")
     blue_button.click()
     time.sleep(1)
 
 driver.quit()
+
 
 
 #**Задание 5. Клик по кнопке с CSS-классом**
