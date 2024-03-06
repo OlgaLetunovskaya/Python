a3607f5aa0373689bd3b3267507ef6ff270ac7aa
Ольга Летуновская <los12ru@internet.ru>
2024-03-06 15:30:43 +0300

    Update lesson_5_task_4.py

--- lesson5/lesson_5_task_4.py
+++ lesson5/lesson_5_task_4.py
@@ -37,22 +37,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.firefox.service import Service as FirefoxService
 service = FirefoxService(executable_path = GeckoDriverManager().install())
 driver = webdriver.Firefox(service = service)
-
+driver.maximize_window()
 
 # Открываем страницу http://uitestingplayground.com/dynamicid
 driver.get("http://uitestingplayground.com/dynamicid")
 
 # Кликаем на синюю кнопку
-blue_button = WebDriverWait(driver, 10).until(
+blue_button = WebDriverWait(driver, 30).until(
     EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn-primary")))
 blue_button.click()
 
 # Запускаем скрипт 3 раза подряд
 for i in range(3):
-    driver = webdriver.Firefox(service = service)
     driver.get("http://uitestingplayground.com/dynamicid")
     blue_button = driver.find_element(By.CSS_SELECTOR, ".btn-primary")
     blue_button.click()
     time.sleep(2)
-
 driver.quit()
