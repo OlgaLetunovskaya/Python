64871cfdecf97de0ce40428ab0e83f06301aa66f
Ольга Летуновская <los12ru@internet.ru>
2024-03-07 08:52:08 +0300

    Update lesson_5_task_5.py

--- lesson5/lesson_5_task_5.py
+++ lesson5/lesson_5_task_5.py
@@ -31,17 +31,14 @@ driver.quit()
 
 import time
 from selenium import webdriver
-from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.firefox.service import Service as FirefoxService
-service = FirefoxService(executable_path = GeckoDriverManager().install())
-driver = webdriver.Firefox(service = service)
+driver = webdriver.Firefox()
 driver.maximize_window()
 
 # Открваем страницу http://uitestingplayground.com/classattr.
-driver = webdriver.Firefox()
+
 driver.get("http://uitestingplayground.com/classattr")
 
 # Кликаем на синюю кнопку
