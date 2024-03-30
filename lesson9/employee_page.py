24a4bf498d2aafdb81801f7db6a8b90a87fe4521
Ольга Летуновская <los12ru@internet.ru>
2024-03-30 16:06:41 +0300

    Update employee_page.py

--- lesson9/employee_page.py
+++ lesson9/employee_page.py
@@ -5,15 +5,21 @@ class EmployeePage:
 
     def __init__(self, token):
         self.headers = {'Authorization': f'Bearer {token}'}
-
+    #GET
     def get_employees(self):
         return requests.get(self.BASE_URL, headers=self.headers)
+        assert response.status_code == 200
 
+    #POST
     def add_employee(self, employee_data):
         return requests.post(self.BASE_URL, json=employee_data, headers=self.headers)
 
+    #GET
     def get_employee(self, employee_id):
         return requests.get(f"{self.BASE_URL}/{employee_id}", headers=self.headers)
 
+    #PATCH
     def update_employee(self, employee_id, employee_data):
         return requests.patch(f"{self.BASE_URL}/{employee_id}", json=employee_data, headers=self.headers)
+
+
